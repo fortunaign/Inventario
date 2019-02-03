868d05c7111e673f1e11b25f5078eb231813961d
Ignacio Fortuna Guzman <ignaciofortuna.inc@hotmail.com>
2019-02-03 16:12:44 -0400

    farmacia - formulario
    Forumario farmacia parte 1

--- form/farmacia/inProd.php
+++ form/farmacia/inProd.php
@@ -9,5 +9,27 @@
 <body>
     <h1><a href="/Inventario/form/farmacia/farmacia.php">Farmacia</a></h1>
     <h2>Entrada Producto</h2>
+    <label for="">Nombre</label><br>
+    <input type="text" name="proname" id=""><br>
+    <label for="">Concentracion</label><br>
+    <label for="">Forma Farmaceutica</label><br>
+    <label for="">Forma de presentacion</label><br>
+    <label for="">Numero de lote</label><br>
+    <input type="text" name="pronumlot" id=""><br>
+    <label for="">Fecha Vencimiento</label><br>
+    <input type="date" name="vencimiento" id=""><br>
+    <label for="">Registro Sanitario</label><br>
+    <input type="text" name="rsanitario" id=""><br>
+    <label for="">Fabricante</label><br>
+    <label for="">Importador</label><br>
+    <label for="">Condisiones de almacenamiento</label><br>
+    <label for="">Costo</label><br>
+    <input type="text" name="procosto" id=""><br>
+    <label for="">Cantidad</label><br>
+    <input type="number" name="cantidad" id=""><br>
+    <label for="">Unto de Reorden</label><br>
+    <input type="number" name="puntoreo" id=""><br>
+    <label for="">Estado</label>
+
 </body>
 </html>
