aa38db2b560905e1ec045fd2d5a7258f199816d7
Ignacio Fortuna Guzman <ignaciofortuna.inc@hotmail.com>
2019-02-03 15:57:40 -0400

    Farmacia inicio
    Inventario de farmacia.

---
+++ form/farmacia/inProd.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Farmacia</title>
+</head>
+<body>
+    <h1><a href="/Inventario/form/farmacia/farmacia.php">Farmacia</a></h1>
+    <h2>Entrada Producto</h2>
+</body>
+</html>
